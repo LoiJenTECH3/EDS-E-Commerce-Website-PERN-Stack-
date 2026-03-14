c154b8e56f350dee08e26f0bc317775b8fbf9966
LoiJenTECH3 <loijentech3@gmail.com>
2026-03-14 17:10:14 +0800

    feat: add product management features and enhance UI components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,32 +9,29 @@ import EditProductPage from "./pages/EditProductPage";
 import useAuthReq from "./hooks/useAuthReq";
 import useUserSync from "./hooks/useUserSync";
 
- 
 function App() {
   const { isClerkLoaded, isSignedIn } = useAuthReq();
   useUserSync();
 
   if (!isClerkLoaded) return null;
-  
-  return (
-      <div className="min-h-screen bg-base-100">
-        <Navbar />
-        <main className="max-w-5xl mx-auto px-4 py-8">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/product/:id" element={<ProductPage />} />
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/create" element={<CreatePage />} />
-            <Route path="/edit/:id" element={<EditProductPage />} />
 
-          </Routes>
-        </main>
-        
-        
-      </div>
+  return (
+    <div className="min-h-screen bg-base-100">
+      <Navbar />
+      <main className="max-w-5xl mx-auto px-4 py-8">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/product/:id" element={<ProductPage />} />
+          <Route path="/profile" element={isSignedIn ? <ProfilePage /> : <Navigate to={"/"} />} />
+          <Route path="/create" element={isSignedIn ? <CreatePage /> : <Navigate to={"/"} />} />
+          <Route
+            path="/edit/:id"
+            element={isSignedIn ? <EditProductPage /> : <Navigate to={"/"} />}
+          />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
 export default App;
-
-
